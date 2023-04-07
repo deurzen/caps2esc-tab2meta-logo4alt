1cb3edd87ea31c55bbae54509ee93f462438d755
deurzen <max@deurzen.net>
2023-04-08 00:41:03 +0200

    add tab2meta functionality

--- caps2esclogo4alt.c
+++ caps2esclogo4alt.c
@@ -8,8 +8,14 @@ const struct input_event
 syn       = { .type = EV_SYN, .code = SYN_REPORT,   .value = 0 },
 esc_up    = { .type = EV_KEY, .code = KEY_ESC,      .value = 0 },
 ctrl_up   = { .type = EV_KEY, .code = KEY_LEFTCTRL, .value = 0 },
+meta_up   = { .type = EV_KEY, .code = KEY_LEFTMETA, .value = 0 },
+alt_up    = { .type = EV_KEY, .code = KEY_LEFTALT,  .value = 0 },
+tab_up    = { .type = EV_KEY, .code = KEY_TAB,      .value = 0 },
 esc_down  = { .type = EV_KEY, .code = KEY_ESC,      .value = 1 },
-ctrl_down = { .type = EV_KEY, .code = KEY_LEFTCTRL, .value = 1 };
+ctrl_down = { .type = EV_KEY, .code = KEY_LEFTCTRL, .value = 1 },
+meta_down = { .type = EV_KEY, .code = KEY_LEFTMETA, .value = 1 },
+alt_down  = { .type = EV_KEY, .code = KEY_LEFTALT,  .value = 1 },
+tab_down  = { .type = EV_KEY, .code = KEY_TAB,      .value = 1 };
 
 static inline int
 read_event(struct input_event *event)
@@ -45,7 +51,9 @@ main(void)
     enum {
         START,
         CAPSLOCK_HELD,
-        CAPSLOCK_IS_CTRL
+        CAPSLOCK_IS_CTRL,
+        TAB_HELD,
+        TAB_IS_META,
     } state = START;
     struct input_event input;
 
@@ -65,6 +73,9 @@ main(void)
             if (input.type == EV_KEY && input.code == KEY_CAPSLOCK &&
                 input.value)
                 state = CAPSLOCK_HELD;
+            else if (input.type == EV_KEY && input.code == KEY_TAB &&
+                input.value)
+                state = TAB_HELD;
             else
                 intercept_event(&input);
             break;
@@ -96,6 +107,35 @@ main(void)
             } else
                 intercept_event(&input);
             break;
+        case TAB_HELD:
+            if (input.type == EV_KEY && input.code == KEY_TAB) {
+                if (input.value == 0) {
+                    write_event(&tab_down);
+                    write_event(&syn);
+                    usleep(delay);
+                    write_event(&tab_up);
+                    state = START;
+                }
+            } else if ((input.type == EV_KEY && input.value == 1) ||
+                       input.type == EV_REL || input.type == EV_ABS) {
+                write_event(&meta_down);
+                write_event(&syn);
+                usleep(delay);
+                intercept_event(&input);
+                state = TAB_IS_META;
+            } else
+                intercept_event(&input);
+            break;
+        case TAB_IS_META:
+            if (input.type == EV_KEY && input.code == KEY_TAB) {
+                input.code = KEY_LEFTMETA;
+                write_event(&input);
+                if (input.value == 0)
+                    state = START;
+            } else
+                intercept_event(&input);
+            break;
+        default: break;
         }
     }
 }
